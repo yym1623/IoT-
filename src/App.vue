8342a36a2c0a466c54c55fd012d493ef172ba880
yym1623 <rhenfla0312@github.com>
2024-12-11 11:09:24 +0900

    디자인 수정

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
       </div>
       
       <div class=" lg:flex lg:flex-1 lg:justify-end">
-        <span class="cursor-pointer material-symbols-outlined">move</span>
+        <span class="cursor-pointer material-symbols-outlined move-icon">move</span>
         <!-- <a href="#" class="font-semibold text-gray-900 text-sm/6">Log in <span aria-hidden="true">&rarr;</span></a> -->
       </div>
     </nav>
@@ -17,8 +17,8 @@
     <!-- Item Section -->
      <div class="px-5 divide-y divide-gray-100">
        <ul role="list" >
-         <li class="flex justify-between py-5 gap-x-6">
-           <div class="flex min-w-0 gap-x-4">
+         <li class="flex items-center justify-between py-5 gap-x-6">
+           <div class="flex items-center min-w-0 gap-x-4">
             <!-- data -->
             <span @click="on('1')" v-if="!lightCheck" class="material-symbols-outlined">lightbulb</span>
             <span @click="off('1')" v-else  class="material-symbols-outlined">emoji_objects</span>
@@ -29,8 +29,8 @@
             <ToggleSwitch  v-model="lightCheck" />
            </div>
          </li>
-         <li class="flex justify-between py-5 gap-x-6">
-           <div class="flex min-w-0 gap-x-4">
+         <li class="flex items-center justify-between py-5 gap-x-6">
+           <div class="flex items-center min-w-0 gap-x-4">
             <!-- data -->
             <span @click="on('2')" v-if="!airconCheck" class="material-symbols-outlined">lightbulb</span>
             <span @click="off('2')" v-else  class="material-symbols-outlined">emoji_objects</span>
@@ -41,8 +41,8 @@
             <ToggleSwitch  v-model="airconCheck" />
            </div>
          </li>
-         <li class="flex justify-between py-5 gap-x-6">
-           <div class="flex min-w-0 gap-x-4">
+         <li class="flex items-center justify-between py-5 gap-x-6">
+           <div class="flex items-center min-w-0 gap-x-4">
             <!-- data -->
             <span @click="on('3')" v-if="!beamProjector1" class="material-symbols-outlined">lightbulb</span>
             <span @click="off('3')" v-else  class="material-symbols-outlined">emoji_objects</span>
@@ -96,20 +96,21 @@ const off = (type: string) => {
 <style lang="scss" scoped>
 
 .container {
-  background: #83a4d4;  /* fallback for old browsers */
-background: -webkit-linear-gradient(to right, #b6fbff, #83a4d4);  /* Chrome 10-25, Safari 5.1-6 */
-background: linear-gradient(to right, #b6fbff, #83a4d4); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  background: #360033; 
+  background: -webkit-linear-gradient(to right, #403ea7, #0c818b);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #403ea7, #0c818b); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  color: #fff;
+  .logo {
+    font-size: 20px; /* Adjust size as needed */
+    font-weight: bold;
+  }
 
-}
-.logo {
+  .light-logo {
+    font-size: 16px;
+  }
 
-  color: #222425; /* Replace with your desired blue color */
-  font-size: 20px; /* Adjust size as needed */
-  font-weight: bold;
-}
-
-.light-logo {
-  color: #222425;
-  font-size: 16px;
+  .move-icon {
+    font-size: 33px;
+  }
 }
 </style>
